7a38e9c4bf1e22cff45ecd193b2e154e571ca718
iusup12 <145194543+iusup12@users.noreply.github.com>
2024-07-11 21:43:33 +0300

    Delete Said_iusup_hmw3 directory

--- Said_iusup_hmw3/app/build/generated/ap_generated_sources/debug/out/com/example/Iusup_hmw3/BR.java
+++
@@ -1,5 +0,0 @@
-package com.example.Iusup_hmw3;
-
-public class BR {
-  public static final int _all = 0;
-}
--- Said_iusup_hmw3/app/build/generated/data_binding_trigger/debug/com/example/Iusup_hmw3/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.example.Iusup_hmw3;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
